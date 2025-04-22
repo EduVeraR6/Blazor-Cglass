10630fa7708326db68d5f31249a5647e31571c00
Eduardo <vedison317@gmail.com>
2025-04-22 16:30:31 -0500

    Add search generico

--- Models/Utils.cs
+++ Models/Utils.cs
@@ -6,4 +6,10 @@
         Creando,
         Editando
     }
+
+    public class SelectOption
+    {
+        public string Value { get; set; }
+        public string Label { get; set; }
+    }
 }
---
+++ Shared/Components/SearchComponent.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using PruebaBlazor.Models;
+
+namespace PruebaBlazor.Shared.Components
+{
+    public partial class SearchComponent : ComponentBase
+    {
+        [Parameter] public string InputType { get; set; } = "text"; 
+        [Parameter] public string SearchValue { get; set; }
+        [Parameter] public EventCallback<string> OnSearch { get; set; }
+
+        [Parameter] public List<SelectOption>? Options { get; set; }
+
+        private async Task HandleKeyUp(KeyboardEventArgs e)
+        {
+            if (e.Key == "Enter")
+            {
+                await OnSearch.InvokeAsync(SearchValue);
+            }
+        }
+
+        private async Task HandleSelectChange(ChangeEventArgs e)
+        {
+            SearchValue = e.Value?.ToString();
+            await OnSearch.InvokeAsync(SearchValue);
+        }
+
+      
+    }
+}
